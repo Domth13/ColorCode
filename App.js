03bbd156bbb6b4f0f364783534d75d7b3d3a3b90
Domth13 <domth13@gmail.com>
2022-08-15 21:26:49 +0200

    upload

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { StateProvider } from './src/hooks/useState';
+
+import GameStart from './src/pages/GameSetup'
+import GamePage from './src/pages/GamePage'
+import GameEnd from './src/pages/GameEnd'
+import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    
+    return (
+        <StateProvider >
+            <NavigationContainer >
+                <Stack.Navigator 
+                  initialRouteName='GameStart'
+                  screenOptions={{ headerShown: false}} 
+                  >
+                    <Stack.Screen name='GameStart' component={GameStart} />
+                    <Stack.Screen name='GamePage' component={GamePage} />
+                    <Stack.Screen name='GameEnd' component={GameEnd} />
+                </Stack.Navigator>
+                {<ExpoStatusBar style='auto'/>}
+            </NavigationContainer>
+        </StateProvider>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
